558e4ebd3aec7cbaf4bbac66ef8ab422445fc1dd
Anthonymend <supersecret909@gmail.com>
2024-09-22 18:28:06 -0700

    setup

---
+++ src/vectorMath.cpp
@@ -0,0 +1,10 @@
+#include <math.h>
+
+float vectorMag();
+float Vectoradd(int a, int b);
+float Vectorsubtract(int a, int b);
+float Vectormultiply(int a, int b);
+float Vectordivide(int a, int b);
+float VectordotProduct();
+float VectorcrossProduct();
+float VectorangleBetween();
---
+++ src/vectorMath.h
@@ -0,0 +1,14 @@
+#ifndef vectorMath
+#define vectorMath
+
+float vectorMag
+
+float Vectoradd(int a, int b);
+float Vectorsubtract(int a, int b);
+float Vectormultiply(int a, int b);
+float Vectordivide(int a, int b);
+float VectordotProduct();
+float VectorcrossProduct();
+float VectorangleBetween();
+
+#endif
