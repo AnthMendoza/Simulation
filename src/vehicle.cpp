40ef979e12853d68150957030c8fbc5de3921c41
Anthonymend <supersecret909@gmail.com>
2024-09-24 16:31:40 -0700

    creating vehicle class

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
+#include "../include/vehicle.h"
 
 
 
 
-class Vehicle{
-    public:
-        float   Xposition , Yposition , Zposition;     // position
-        float    roll , pitch, yaw ;    // rotation
+Vehicle::Vehicle(float x, float y, float z, float mroll, float mpitch, float myaw)
+    : Xposition(x), Yposition(y), Zposition(z), roll(mroll), pitch(mpitch), yaw(myaw) {}
 
-    Vehicle(float x, float y, float z, float mroll, float mpitch, float myaw){
-        Xposition = x;
-        Yposition = y;
-        Zposition = z;
-        roll = mroll;
-        pitch = mpitch;
-        yaw = myaw;
-
-    }
-
-    void display(){
-         std::cout << "Position: (" << Xposition << ", " << Yposition << ", " << Zposition << ")\n"<< "Orientation (Roll, Pitch, Yaw): (" << roll << ", " << pitch << ", " << yaw << ")\n";
-    }
-};
+// Method implementation to display the vehicle's state
+void Vehicle::display() {
+    std::cout << "Position: (" << Xposition << ", " << Yposition << ", " << Zposition << ")\n"
+              << "Orientation (Roll, Pitch, Yaw): (" << roll << ", " << pitch << ", " << yaw << ")\n";
+}
