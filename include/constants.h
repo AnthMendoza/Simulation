33e81dea10b7092e7963d86ead1434f4b8029415
Anthonymend <supersecret909@gmail.com>
2024-10-06 04:42:19 -0700

    adding data csv

--- include/constants.h
+++ include/constants.h
@@ -6,9 +6,10 @@ namespace constants{
     float gravitationalAcceleration = -9.81; 
 
     std::array<float,3> initPosition = {0,295528.164398,110000};
+    //std::array<float,3> initPosition = {0,0,1000};
     std::array<float,3> wind = {0,0,0}; //  m/s x,y,z  this will be stacked on top of whatever the current velocity is on an absolute cordinate system.
     std::array<float,3> initVehicleState = {0,1,0}; // this the vehicle vector, its direction is the directionthe nose points. it will be normilized, magnatiude is meain
-    std::array<float , 3> MOI = {2,2,2};
+    std::array<float , 3> MOI = {25,25,25};
     std::array<float,3> initVelocity = {0,-2000,0}; // m/s
     
     float centerOfPressure = 1;
