0c9f709cf73a16547ebdc0838c086dfecfea15d5
Anthonymend <supersecret909@gmail.com>
2024-11-22 15:15:31 -0800

    adding kinematics to gimbal

--- src/control.cpp
+++ src/control.cpp
@@ -174,11 +174,11 @@ void landingBurn(Vehicle &rocket){
 
 
 
-    rocket.engineGimbal( PIDOutputY , -PIDOutputX );
+    rocket.engineGimbal( -PIDOutputY , PIDOutputX );
     
     
 
-    direction[0] = rocket.gimbalY;
+    //direction[0] = rocket.gimbalY;
     direction[1] = rocket.gimbalX;
 
 
