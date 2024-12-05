d1ed29e936a573afc7fbfa74fab0375a131e3edc
Anthonymend <supersecret909@gmail.com>
2024-12-05 15:13:03 -0800

    entry burn write up

--- include/RungeKutta.h
+++ include/RungeKutta.h
@@ -1,7 +1,7 @@
 #ifndef RUNGEKUTTA_H
 #define RUNGEKUTTA_H
 
-
+float acceleration(float force , float mass);
 
 void RungeKutta4th(float force, float mass, float timeStep, float &velocity, float &position);
 
--- src/RungeKutta.cpp
+++ src/RungeKutta.cpp
@@ -1,28 +1,28 @@
 #include <cmath>
 #include "../include/RungeKutta.h"
 
+float acceleration(float force , float mass){
+    return force / mass;
+}
 
 void RungeKutta4th(float force, float mass, float timeStep, float &velocity, float &position) {
-
-    auto acceleration = [force, mass](float) -> float {
-        return force / mass;
-    };
+    
 
     // Calculate k1 for velocity and position
-    float k1v = acceleration( velocity) * timeStep;
+    float k1v = acceleration(force , mass) * timeStep;
     float k1x = velocity * timeStep;
 
     // Calculate k2 for velocity and position
-    float k2v = acceleration( velocity + k1v / 2.0f) * timeStep;
-    float k2x = (velocity + k1v / 2.0f) * timeStep;
+    float k2v = (acceleration(force , mass) + k1v / 2.0f) * timeStep;
+    float k2x = (velocity + k1x / 2.0f) * timeStep;
 
     // Calculate k3 for velocity and position
-    float k3v = acceleration( velocity + k2v / 2.0f) * timeStep;
-    float k3x = (velocity + k2v / 2.0f) * timeStep;
+    float k3v = acceleration(force , mass) + k2v / 2.0f * timeStep;
+    float k3x = (velocity + k2x / 2.0f) * timeStep;
 
     // Calculate k4 for velocity and position
-    float k4v = acceleration( velocity + k3v) * timeStep;
-    float k4x = (velocity + k3v) * timeStep;
+    float k4v = (acceleration(force , mass)+ k3v) * timeStep;
+    float k4x = (velocity + k3x) * timeStep;
 
     // weighted averge of the samples
     velocity = velocity + (k1v + 2 * k2v + 2 * k3v + k4v) / 6.0f;
